b7653fe330b35ddaa23386ae06cb3f4ecbed9624
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-08-04 23:54:23 +0530

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,60 @@
+//basics
+
+//data types
+//1. primitive datatypes
+//2. refrence data types.
+
+//primitive data types stores a single value in the memory.
+//examples
+//1.string,boolean true or false && null, undefined,number.
+
+// string data type.
+// let name = "this is hemanth kumar";
+// console.log(name);
+ //console.log(typeof name);
+//it printout the name which is a string.
+
+// undefined.
+
+//let x ;
+// console.log(x);
+//console.log(typeof x);
+
+// null.
+
+// typeof opertor.
+// the type of operator prints the type of the given datatype.
+
+// let x = 100;
+// let name = "lingabathina hemanth kumar"
+
+// console.log(typeof x)
+// console.log(typeof name)
+
+ let bunty = {
+ realname : 'hemamnth kumar lingabathina',
+ age : 25,
+ height : "5'9 inches",
+ }
+ console.log(bunty["realnname"])
+
+// console.log(typeof bunty);
+// //if you want to know the specifc list of the object you can console.log(the name of the object (.) and the list )
+// console.log(bunty.realname);
+// console.log(bunty.age);
+// console.log(bunty.height);
+
+
+//var , let and const.
+
+//adding of 2 integer numebrs in java script.
+//using var let and const.
+
+
+var n1 = 100.0;
+var n2 = 2000;
+
+console.log(typeof n1);
+
+
+
