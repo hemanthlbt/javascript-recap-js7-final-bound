dc68a7044710621982439a14c1e7fabc289f1844
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-08-06 23:46:18 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,17 @@
+console.log("arrays")
+//string arrays
+var namearray = ['hemanth' , 'akhil' ,'jayanth' , 'arshad']
+console.log(namearray)
+//numbers array
+var numarray = [1,2,3,4,5,6,7,8,9,10]
+console.log(numarray)
+//mixed arrays
+var mixed = [1,'bunty','legion',true,null,undefined]
+console.log(mixed)
+//accessing an element in an array
+console.log(namearray[3])
+console.log(numarray[3])
+console.log(mixed[3])
+
+
+
