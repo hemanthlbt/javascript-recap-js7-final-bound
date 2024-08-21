cd1687faf135ea722fdab27d8c4c6d32a1536c66
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-08-21 20:34:48 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,28 @@
+function audi() {
+
+console.log("audi is a good car company")
+
+}
+audi();
+document.cookie = "username=hemanth";
+
+
+// how do we define a function 
+//1. Function decleration 
+//2. function expression
+//: named function.
+// anonomous function expression.
+// arrow function expression.
+
+
+//function declaration
+
+function hemanth() {
+
+console.log('hemanth is a very good boy')
+
+}
+
+hemanth()
+
+
