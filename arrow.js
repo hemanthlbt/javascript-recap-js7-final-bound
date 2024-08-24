8af027cb8121c285580a34994c6f3f110a8cdc0a
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-08-24 18:02:09 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,22 @@
+//these are arrow fucntions
+
+// let hemanth = function () {
+
+
+
+//   console.log("this is a fucntion expression")
+// }
+
+// hemanth()
+
+//This is a arrow function
+
+let hemanth = () => {
+
+  console.log("this is a motherfucking arrow fucntion")
+
+
+}
+//andcall the motherfucking function 
+
+hemanth()
