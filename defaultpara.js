abfcfbb9380efffac9b48144eb0c4f612aa212a1
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-08-25 18:17:28 +0530

    Add files via upload
    parameteres and default parameteres!!!!!!!

---
+++ defaultpara.js
@@ -0,0 +1,31 @@
+// here i am trying to figure out what the hell is a default parameter is...
+// so basically
+
+// let  games = (a=100,b=200) => {
+
+// console.log(a*b)
+
+
+// }
+
+// games()
+
+//here is another example of this..
+
+
+let hemanth = (x,y=100) => {
+
+  console.log(x+y)
+
+}
+
+hemanth(4)
+
+
+
+
+
+
+
+
+
---
+++ parameters.js
@@ -0,0 +1,30 @@
+// so i am gussing these are parameters.
+
+// let hemanth = (a,b,c) => {
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// }
+
+
+// hemanth (12,23,34)
+
+
+// so now lets try to code default parameters.
+
+
+// let nigga = (a=100,b=200,c=300) => {
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+
+// }
+
+
+// nigga()
