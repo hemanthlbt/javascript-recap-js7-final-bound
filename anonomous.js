afa3de202f8e86b80ba3a390cdcf32a4424f7d85
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-08-25 17:44:19 +0530

    Add files via upload
    Anonomous functions in java script with examples!

---
+++ anonomous.js
@@ -0,0 +1,15 @@
+//these are functions but anonomous.
+// a function with out a name is called an anonomous funciton
+let bunty = function ()     {
+
+
+console.log("this is anonomous function")
+
+
+
+
+}
+
+
+
+bunty()
