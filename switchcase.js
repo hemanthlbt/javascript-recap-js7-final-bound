354d813aed278c0507984759beaf2ac57d682a7b
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-08-26 14:12:21 +0530

    Add files via upload
    This is a switch case..!

---
+++ switchcase.js
@@ -0,0 +1,36 @@
+function esha() {
+
+
+
+
+var weeks = 4 
+switch (weeks) {
+case 1: 
+console.log("monday")
+break;
+case 2: 
+console.log("tuesday")
+break;
+case 3: 
+console.log("wednesday")
+break;
+case 4: 
+console.log("thrusday")
+break;
+case 5: 
+console.log("friday")
+break;
+case 6: 
+console.log("saturday")
+break;
+case 7: 
+console.log("sunday")
+break;
+}
+
+}
+esha()
+
+
+
+
