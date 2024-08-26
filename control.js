da94c21b072b736253676f84e6fd016ba6cac952
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-08-26 12:29:12 +0530

    Add files via upload
    control stetements

---
+++ control.js
@@ -0,0 +1,19 @@
+var marks = 2
+
+if (marks > 100) {
+  console.log("awsome")
+}
+
+else if (marks < 20 && marks < 15) {
+  console.log("call your parents")
+}
+
+else if (marks < 50) {
+  console.log("better luck next time")
+}
+
+else if( marks > 130) {
+  console.log("great work!")
+}
+
+
