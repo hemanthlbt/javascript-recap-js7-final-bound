1995e44ccd026c8f10ee629f2978567be61d5fc5
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-08-27 19:18:37 +0530

    Add files via upload
    all types of loops in java script

---
+++ typesloops.js
@@ -0,0 +1,56 @@
+// for loop
+// while loop
+// do while loop
+// infinity loop
+// break
+// continue
+// for in
+// without using loop
+
+// for loop.
+// var i = 1;
+// for (i=1;i<99;i++) {
+// console.log(i)
+// }
+// while loop.
+// var i = 1;
+// while (i=1) {
+// console.log(i)
+// i++
+// }
+//trying a do while loop
+
+var i = 0;
+
+
+do {
+
+	console.log(i)
+	i++
+}
+
+while(i<11)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
