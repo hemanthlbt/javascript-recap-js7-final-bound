246f4b21ef18bfcc3ebac545d0ed4eecc4c895de
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-09-27 17:33:04 +0530

    Add files via upload
    enough is enough!!!!!!

---
+++ lasttime.js
@@ -0,0 +1,28 @@
+//functions 
+
+
+// named function 
+  function hemanth()  {
+  	console.log("this is hemanth kumar")
+}
+hemanth()
+
+
+//anonomous function 
+let hemanthkumar = function()  {
+
+console.log("this is also hemanth kumar")
+}
+hemanthkumar()
+
+
+//arrow funciton 
+
+
+let eshanarang = () => {
+console.log("this is samara saigal")
+}
+eshanarang()
+
+
+
