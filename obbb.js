d76b3a3774b6befca9b862732475f171c72e3912
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-09-27 14:32:10 +0530

    Add files via upload

---
+++ obbb.js
@@ -0,0 +1,26 @@
+//spread operator for objects
+
+
+const per1 = {
+
+
+name : "esha"
+
+}
+
+
+const per2 = {
+
+name : "samarasaigal"
+
+}
+
+
+const perper = {...per1,...per2};	
+
+
+console.log(perper)
+
+
+
+
