5584e54a42ebff4bf17574ba08131b95c0f6e80f
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-01 00:11:05 +0530

    Add files via upload
    1st upload on october 1st 2024 the end of my goals!

--- time.js
+++ time.js
@@ -1,15 +1,30 @@
 // date objects
 
-var bunty = new Date()
-console.log(bunty)
+// var bunty = new Date()
+// console.log(bunty)
 
-// default
-var two = new Date(2024,2,22)
-console.log(two)
+// // default
+// var two = new Date(2024,2,22)
+// console.log(two)
 
-// string data types and custom date
+// // string data types and custom date
 
-var three = new Date("2024 3 22")
-console.log(three)
+// var three = new Date("2024 3 22")
+// console.log(three)
+
+
+// first commit in october 1st
+
+// let text = `hemanthkumar`
+// console.log(text)
+
+
+
+
+
+
+
+
+
 
 
