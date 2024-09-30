8c21b2365899bbc540abdcb113c7131786e1bea6
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-01 00:31:49 +0530

    Add files via upload

---
+++ ope24.js
@@ -0,0 +1,76 @@
+//java script operators
+
+// additional operatorrs
+
+
+
+//arthematic operators
+//assignment operators
+//comparission operators
+//ternary and logical operators
+
+// arthematic ==> additional
+
+let x = 100
+let y = 200
+console.log(x+y)
+//==> subtraction 
+let bunty  = 3000
+let hemanth = 500
+
+console.log(hemanth-bunty)
+
+//==> multiplication
+
+let esha = 200
+let samara = 500
+
+console.log(esha*samara)
+
+//==> divisional operator
+
+let eshanarang = 50
+let samarasaigal = 50
+console.log(eshanarang/samarasaigal)
+
+//==> remainder, exponential, incerement and decrement operatotr
+
+let kumar = 500
+let swaroop = 50
+console.log(kumar%swaroop)
+
+//==> exponential
+
+let deepika = 3
+console.log(deepika**2)
+
+//==> incremential operator
+
+let bhai = 100
+bhai++
+console.log(bhai)
+
+
+//==> decremental operator
+let allu = 100
+allu--
+console.log(allu)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
