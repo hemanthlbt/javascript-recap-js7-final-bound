032cebbda07eac9297f05679db02ad18b12b4ce6
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-09-30 15:46:10 +0530

    Add files via upload

--- lasttime.js
+++ lasttime.js
@@ -1,28 +1,75 @@
 //functions 
 
 
+
 // named function 
-  function hemanth()  {
-  	console.log("this is hemanth kumar")
+//   function hemanth()  {
+//   	console.log("this is hemanth kumar")
+// }
+// hemanth()
+
+
+// //anonomous function 
+// let hemanthkumar = function()  {
+
+// console.log("this is also hemanth kumar")
+// }
+// hemanthkumar()
+
+
+// //arrow funciton 
+
+
+// let eshanarang = () => {
+// console.log("this is samara saigal")
+// }
+// eshanarang()
+
+
+// loose equality and strict equality
+
+// let x = 100
+// let y = 300
+
+// if (x!==y) {
+
+// 	console.log("this is a nigga")
+// }
+
+let x = `hemanthkumar`
+let y = 100
+let ultra = null 
+console.log(x)
+console.log(y)
+console.log(ultra)
+
+
+if (x!==y) {
+
+
+	console.log("this is a nigga")
 }
-hemanth()
 
 
-//anonomous function 
-let hemanthkumar = function()  {
-
-console.log("this is also hemanth kumar")
-}
-hemanthkumar()
 
 
-//arrow funciton 
 
 
-let eshanarang = () => {
-console.log("this is samara saigal")
-}
-eshanarang()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
