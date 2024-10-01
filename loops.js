12d6fb69bd5548ada94ba1db409f7628462ef64f
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-01 23:53:24 +0530

    Add files via upload

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 // we have 2 types of loops in java script
 // conditional loops and non conditional loops
-//conditional==> forloop, whileloop, dowhileloop and infinity loop.
+// conditional==> forloop, whileloop, dowhileloop and infinity loop.
 // and in non conditional loops we have ==> for in loop and foroff loop.
 
 
@@ -18,13 +18,175 @@
 // while looping 
 
 
-let i = 9
+// let i = 9
 
-while (i<100){
+// while (i<100){
 
-	i++;
-console.log(i)
+// 	i++;
+// console.log(i)
+// }
+
+// do-while-looping
+
+// ok i got the dowhile looping now i will move to infinity loop.
+
+// infinity loop
+
+// for (var i = 1; i<100;i++){
+
+// console.log(i)
+
+// }
+
+// **break;
+
+
+// let i = 1
+
+// while (true) {
+
+// console.log("number:"+i);
+// i++;
+// }
+
+// for (i=400;i<1000;i++){
+// if (i==410){
+// 	break;
+// }
+
+// console.log("number :" + i);
+
+// }
+
+// with all due and honestey i will creat a new one samarasaigal
+// for (i=1;i<90;i++){
+// if (i==20){
+
+// 	break;
+// }
+
+// console.log("this number is not :" +i)
+
+// }
+
+
+// for (i=1;i<10;i+1){
+
+// if (i==5000){
+
+// break;
+// }
+// console.log(i)
+
+
+// }
+
+
+// this is using break keyword in java script..
+// for (var i=1;i<51;i++) {
+
+// if (i===16) {
+// 	break;
+//  }
+// 	console.log(i)
+
+//  }
+
+// for (var i = 1; i<50;i++) {
+
+
+// if (i==35) {
+// 	continue;
+// }
+
+// console.log(i)
+// }
+
+
+// non conditional loops
+
+// for in loop and for off loop.
+
+// for in loop
+
+var hemanth = {
+
+name1: "akhil",
+name2 : "jayanth",
+name3 :"arshad",
+name4 : "praveen",
+girls: {
+
+p1:"eshanrang",
+p2:"samarasaigal",
+p3:"trishajawahar",
 }
+}
+
+
+for ( key in hemanth) {
+
+	console.log(key);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
