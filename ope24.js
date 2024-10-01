06cfac32c1e92602d7d39f91a9157c7174a2d3cc
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-01 15:33:01 +0530

    Add files via upload

--- ope24.js
+++ ope24.js
@@ -1,135 +1,178 @@
-//java script operators
+// //java script operators
 
-// additional operatorrs
-
-
-//arthematic operators
-//assignment operators
-//comparission operators
-//ternary and logical operators
-
-// arthematic ==> additional
-
-let x = 100
-let y = 200
-console.log(x+y)
-//==> subtraction 
-let bunty  = 3000
-let hemanth = 500
-
-console.log(hemanth-bunty)
-
-//==> multiplication
-
-let esha = 200
-let samara = 500
-
-console.log(esha*samara)
-
-//==> divisional operator
-
-let eshanarang = 50
-let samarasaigal = 50
-console.log(eshanarang/samarasaigal)
-
-//==> remainder, exponential, incerement and decrement operatotr
-
-let kumar = 500
-let swaroop = 50
-console.log(kumar%swaroop)
-
-//==> exponential
-
-let deepika = 3
-console.log(deepika**2)
-
-//==> incremential operator
-
-let bhai = 100
-bhai++
-console.log(bhai)
+// // additional operatorrs
 
 
-//==> decremental operator
-let allu = 100
-allu--
-console.log(allu)
+// //arthematic operators
+// //assignment operators
+// //comparission operators
+// //ternary and logical operators
+
+// // arthematic ==> additional
+
+// let x = 100
+// let y = 200
+// console.log(x+y)
+// //==> subtraction 
+// let bunty  = 3000
+// let hemanth = 500
+
+// console.log(hemanth-bunty)
+
+// //==> multiplication
+
+// let esha = 200
+// let samara = 500
+
+// console.log(esha*samara)
+
+// //==> divisional operator
+
+// let eshanarang = 50
+// let samarasaigal = 50
+// console.log(eshanarang/samarasaigal)
+
+// //==> remainder, exponential, incerement and decrement operatotr
+
+// let kumar = 500
+// let swaroop = 50
+// console.log(kumar%swaroop)
+
+// //==> exponential
+
+// let deepika = 3
+// console.log(deepika**2)
+
+// //==> incremential operator
+
+// let bhai = 100
+// bhai++
+// console.log(bhai)
 
 
-//assignment operator
+// //==> decremental operator
+// let allu = 100
+// allu--
+// console.log(allu)
 
-let reddy = "eshanarang"
-console.log(reddy)
-//this is waht assignment operator is..
 
-//comparission operators
+// //assignment operator
 
-//rational and relational
+// let reddy = "eshanarang"
+// console.log(reddy)
+// //this is waht assignment operator is..
 
-let amitab = 400
-let prabhas = 500
+// //comparission operators
 
-if (amitab<prabhas) {
-	console.log("prabhas is the goat")
+// //rational and relational
+
+// let amitab = 400
+// let prabhas = 500
+
+// if (amitab<prabhas) {
+// 	console.log("prabhas is the goat")
+// }
+
+// let arshad = 500
+// let akhil  = 600
+
+// if (arshad<=600) {
+// 	console.log("akhil is the  goat")
+// }
+
+
+// //equality and inequality
+
+// var zee = null
+// var munna = 100
+
+// if (zee!==munna) {
+// console.log("niggabad")
+// }
+
+// console.log("equality and inequality symbols" + "==/===/!=/!==")
+
+// // Ternary operator
+
+// let age = 15
+// let vote = age >= 18 ? 'having vote' : 'not having vote'
+// console.log(vote);
+
+
+// let aage = 25
+// let drink = aage>35 ? 'candrink' : 'cannotdrink'
+// console.log(drink)
+
+// // logical operator
+
+// let swamy = 200
+// let kumara = 500
+// let samaraa = 9000
+
+// if (swamy&&kumara!==samaraa) {
+
+// console.log("hamara")
+// }
+
+// let ena = 1000
+// let sma = 200
+// let mass = 1900
+// let bamara = 100
+
+// if (ena&&mass==bamara) {
+
+// console.log("lamara")
+
+// }
+
+// // control statements
+// // if else and switch case
+
+
+
+// var jonah = 8900
+
+// if (jonah=8900) {
+
+// 	console.log("johan hill is a nigga")
+// }
+
+
+// switch case
+
+const week = 1
+
+switch(week) {
+case 1:
+	console.log("monday")
+	break;
+case 2:
+	console.log("tuesday")
+	break;
+case 3:
+	console.log("wednesday")
+	break;
+case 4:
+	console.log("thrusday")
+	break;
+case 5:
+	console.log("friday")
+	break;
+case 6:
+	console.log("saturday")
+	break;
+case 7:
+	console.log("sunday")
+	break;
+default:
+	console.log("na lavada lo javascript deenamma puku na modda lo jeevitham")
 }
 
-let arshad = 500
-let akhil  = 600
-
-if (arshad<=600) {
-	console.log("akhil is the  goat")
-}
-
-
-//equality and inequality
-
-var zee = null
-var munna = 100
-
-if (zee!==munna) {
-console.log("niggabad")
-}
-
-console.log("equality and inequality symbols" + "==/===/!=/!==")
-
-// Ternary operator
-
-let age = 15
-let vote = age >= 18 ? 'having vote' : 'not having vote'
-console.log(vote);
-
-
-let aage = 25
-let drink = aage>35 ? 'candrink' : 'cannotdrink'
-console.log(drink)
-
-// logical operator
-
-let swamy = 200
-let kumara = 500
-let samaraa = 9000
-
-if (swamy&&kumara!==samaraa) {
-
-console.log("hamara")
-}
-
-let ena = 1000
-let sma = 200
-let mass = 1900
-let bamara = 100
-
-if (ena&&mass==bamara) {
-
-console.log("lamara")
-
-}
 
 
 
-// control statements
 
-// if else and switch case
+
 
 
 
