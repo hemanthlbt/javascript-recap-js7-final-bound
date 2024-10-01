1fc423d1355c55cf49c9354918641d481260a72b
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-01 15:58:53 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,31 @@
+// we have 2 types of loops in java script
+// conditional loops and non conditional loops
+//conditional==> forloop, whileloop, dowhileloop and infinity loop.
+// and in non conditional loops we have ==> for in loop and foroff loop.
+
+
+
+// conditional loops
+
+// for loop
+
+
+// for (var i=10; i<36; i++){
+// console.log(i)
+// }
+
+
+// while looping 
+
+
+let i = 9
+
+while (i<100){
+
+	i++;
+console.log(i)
+}
+
+
+
+
