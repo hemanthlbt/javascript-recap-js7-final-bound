5ae455a8e86347df7615c035f0a0d8cf81be2a0d
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-20 09:21:37 -0700

    Add files via upload

--- newspread.js
+++ newspread.js
@@ -1,23 +1,6 @@
-let origin = [1,2,3,4,5,6]
-console.log(...origin);
-
-let hemanth = [1,2,3,4,5,6,7,8,9]
-console.log(...hemanth)
-
-const kumar = [...origin,...hemanth]
-console.log(kumar)
-
-const nithin = ["esha","samara","trisha"]
-const rampal = ["tagore","jagarn","sharmila"]
-
-const nithinrampal = [...nithin,...rampal]
-console.log(nithinrampal)
-
-const gali = ["teri"]
-const gali1 = ["maa"]
-const gali2 = ["ki"]
-const gali3 = ["chut"]
-
-const galigaloch = [...gali,...gali1,...gali2,...gali3]
-console.log(galigaloch)
+var hemanth = ["esha"]
+var kumar = ["narang"]
+var natural = [...hemanth,...kumar]
+console.log(natural)
+//this is a spread operator..
 
