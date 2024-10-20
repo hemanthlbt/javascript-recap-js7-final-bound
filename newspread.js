93fdac99763839794ba42be348827b415a653bbb
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-20 10:38:16 -0700

    Add files via upload
    Peyton list!

--- newspread.js
+++ newspread.js
@@ -4,3 +4,23 @@ var natural = [...hemanth,...kumar]
 console.log(natural)
 //this is a spread operator..
 
+
+
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+console.log("i love peyton lsit")
+
